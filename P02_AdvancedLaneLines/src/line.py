254ecacb6c969c9f270b47bcf3d8935eb3cebe03
KhardanOne <hod64@freemail.hu>
2020-11-05 14:16:53 +0100

    P02 minor cosmetics

--- P02_AdvancedLaneLines/src/line.py
+++ P02_AdvancedLaneLines/src/line.py
@@ -11,7 +11,7 @@ class Line:
         # 4 - so-so fit found
         # 5 - perfect fit
         self.quality = quality
-
+        self.lane_info = None               # center offset, line radiuses
         self.side = side                    # 'left' or 'right'
         self.frame = frame_count            # in which frame was the line last time updated
         self.coeffs = fit                   # polynomial coefficients
@@ -49,25 +49,3 @@ class Line:
         max = dists.max()
         sane = 1 if min > 700 and max < 1200 else 0  # in pixels in 2D  ##################################### tune here
         return sane, min, max
-
-        # # was the_quality line detected in the last iteration?
-        # self.detected = False
-        # # x values of the last n fits of the line
-        # self.recent_xfitted = []
-        # #average x values of the fitted line over the last n iterations
-        # self.bestx = None
-        # #polynomial coefficients averaged over the last n iterations
-        # self.best_fit = None
-        # #polynomial coefficients for the most recent fit
-        # self.current_fit = [np.array([False])]
-        # #radius of curvature of the line in some units
-        # self.radius_of_curvature = None
-        # #distance in meters of vehicle center from the line
-        # self.line_base_pos = None
-        # #difference in fit coefficients between last and new fits
-        # self.diffs = np.array([0,0,0], dtype='float')
-        # #x values for detected line pixels
-        # self.allx = None
-        # #y values for detected line pixels
-        # self.ally = None
-
